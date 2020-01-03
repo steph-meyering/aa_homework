dba34f41302fc6b880b92b266d92c172030c2b07
Steph Meyering <stephane.meyering@gmail.com>
2020-01-02 23:41:04 -0800

    Created Queue class and related methods

--- w3d5/diy_adt.rb
+++ w3d5/diy_adt.rb
@@ -15,3 +15,22 @@
         @stack[-1]
     end
   end
+
+  class Queue
+
+    def initialize
+        @queue = []
+    end
+
+    def enqueue(el)
+        @queue << el
+    end
+
+    def dequeue
+        @queue.shift
+    end
+    
+    def peek
+        @queue[0]
+    end
+  end
